fdb1f2277eb141a65f18f6e39419bfcd355d1bf2
Chongliang Luo <luocl3009@gmail.com>
2025-10-16 14:22:39 -0500

    update simu-0
    add test data prediction

--- simu-0.R
+++ simu-0.R
@@ -10,5 +10,11 @@ Rcpp::sourceCpp("src/cvrsolver.cpp")
 source("R/myfunctions.R")
 dd0 = readRDS('data/CVR_Cox_simu_data.RDS')
 
+# fit CVR-Cox
 fit0 = CVR(Y=dd0$y[,1], Xlist=list(dd0$X1, dd0$X2), event=dd0$y[,2], rankseq=2, neta =10, nlam = 50,  
            Lamseq = NULL, family = "cox", Wini = NULL, penalty = "GL1" )
+
+# predict test data, output Uno' concordance
+ff = fit0$cvout$cvr.fit
+pred1 = PredCVR(dd0$ytest[,1], dd0$X1test, dd0$X2test, dd0$ytest[,2], ff$W[[1]], ff$W[[2]], alpha = ff$alpha, beta =ff$beta, 
+                Y=dd0$y[,1], X1=dd0$X1, X2=dd0$X2, event=dd0$y[,2], family = "cox", refit = T, combine = T, type.measure = 'UnoC')
